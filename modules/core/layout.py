57a60a76e2952781ad01ff3bb7fdf09086293c35
tnieuweboer-prog <t.nieuweboer@atlascollege.nl>
2025-12-21 15:54:05 +0100

    Update layout.py 1

--- modules/core/layout.py
+++ modules/core/layout.py
@@ -1,1 +1,14 @@
+# modules/core/layout.py
+from flask import session
+
+def inject_globals(app):
+    @app.context_processor
+    def _inject():
+        return {
+            "current_user": {
+                "name": session.get("user"),
+                "role": session.get("role"),
+                "is_admin": session.get("is_admin", False),
+            }
+        }
 
