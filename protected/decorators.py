dde524e914f8b50aa233906f51a0c87aba4812da
tnieuweboer-prog <t.nieuweboer@atlascollege.nl>
2025-12-23 18:31:18 +0100

    Update decorators.py 1

--- protected/decorators.py
+++ protected/decorators.py
@@ -1,1 +1,14 @@
+# protected/admin/decorators.py
+from functools import wraps
+from flask import session, redirect, url_for, request
+
+def admin_required(view):
+    @wraps(view)
+    def wrapped(*args, **kwargs):
+        if session.get("is_admin") is True:
+            return view(*args, **kwargs)
+
+        # onthoud waar iemand heen wilde
+        return redirect(url_for("admin.admin_login", next=request.path))
+    return wrapped
 
